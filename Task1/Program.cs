5e05ead82378e49286232b023d8f06ba17fa6fd4
aarya-ghimire <aaryaghimire36@gmail.com>
2025-12-03 10:48:16 +0545

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle();
+
+        rect.Length = 10;
+        rect.Breadth = 5;
+
+        Console.WriteLine("Area: " + rect.GetArea());
+        Console.WriteLine("Perimeter: " + rect.GetPerimeter());
+        Console.WriteLine(rect.SeeDetails());
+
+        Console.ReadLine();
+    }
+}
